7b8d6e45941143dd79fc9d32adf32e77286e5b9e
Philip Klostermann <philip.klostermann@gmail.com>
2016-09-10 16:55:06 -0400

    implement fetch-first-page-token

--- src/lib/fetch-first-page-token.js
+++ src/lib/fetch-first-page-token.js
@@ -1,3 +1,4 @@
+import cheerio from 'cheerio'
 import requestImport from './request'
 import { buildWatchFragmentsUrl } from './url-builder'
 
@@ -10,14 +11,59 @@ export default function (videoId, session, deps = {}) {
   }
 
   const { request = requestImport } = deps
-  return fetchCommentFragment(videoId, session, request)
+  return fetchCommentsFragment(videoId, session, request)
     .then(extractPageToken)
 }
 
-export function fetchCommentFragment (videoId, session, request) {
-  
+export function fetchCommentsFragment (videoId, session, request) {
+  if (!videoId) {
+    return Promise.reject('Missing first parameter: videoId')
+  }
+  if (!session) {
+    return Promise.reject('Missing second parameter: session')
+  }
+  if (!request) {
+    return Promise.reject('Missing third parameter: request')
+  }
+
+  const { sessionToken, commentsToken } = session
+  if (!sessionToken) {
+    return Promise.reject('Missing "sessionToken" in session parameter')
+  }
+  if (!commentsToken) {
+    return Promise.reject('Missing "commentsToken" in session parameter')
+  }
+
+  const url = buildWatchFragmentsUrl(videoId, commentsToken)
+  const form = { session_token: sessionToken }
+
+  return request({
+    method: 'POST',
+    json: true,
+    url,
+    form
+  })
 }
 
-export function extractPageToken (html) {
+export function extractPageToken (response) {
+  if (!response) {
+    throw new Error('Missing parameter: response')
+  }
 
+  const html = response['watch-discussion']
+  if (!html) {
+    throw new Error('Missing field in response: watch-discussion')
+  }
+
+  const $ = cheerio.load(html)
+  const $btn = $('button[data-menu_name="newest-first"]')
+  if (!$btn.length) {
+    throw new Error('Cannot find page token button element.')
+  }
+
+  const pageToken = $btn.attr('data-token')
+  if (!pageToken) {
+    throw new Error('Button element is missing the data-token attribute.')
+  }
+  return pageToken
 }
--- src/test/lib/fetch-first-page-token.js
+++ src/test/lib/fetch-first-page-token.js
@@ -2,11 +2,13 @@ import test from 'blue-tape'
 import sinon from 'sinon'
 
 import fetchFirstPageToken, {
-  fetchCommentFragment,
+  fetchCommentsFragment,
   extractPageToken
 } from '../../lib/fetch-first-page-token'
 
-test('/lib/fetch-first-page-token.js', t => {
+import { buildWatchFragmentsUrl } from '../../lib/url-builder'
+
+test.only('/lib/fetch-first-page-token.js', t => {
   t.test('- module exports a function', t => {
     t.equal(typeof fetchFirstPageToken, 'function', 'is of type function')
     t.end()
@@ -19,21 +21,13 @@ test('/lib/fetch-first-page-token.js', t => {
     t.end()
   })
 
-  t.test('- requires a videoId parameter', t => {
+  t.test('- rejects promise if required parameters missing', t => {
     return fetchFirstPageToken()
       .then(() => t.fail('promise should not resolve'))
       .catch((err) => {
         t.ok(err, 'promise is rejected with an error')
       })
       .then(() => fetchFirstPageToken(null, {}))
-      .then(() => t.fail('promise should not resolve'))
-      .catch((err) => {
-        t.ok(err, 'promise is rejected with an error')
-      })
-  })
-
-  t.test('- requires a session parameter', t => {
-    return fetchFirstPageToken()
       .then(() => t.fail('promise should not resolve'))
       .catch((err) => {
         t.ok(err, 'promise is rejected with an error')
@@ -45,5 +39,149 @@ test('/lib/fetch-first-page-token.js', t => {
       })
   })
 
+  t.test('- fetchCommentsFragment() returns a promise', t => {
+    const returnValue = fetchCommentsFragment().catch(() => {})
+    t.ok(returnValue.then, 'return value has .then')
+    t.equal(typeof returnValue.then, 'function', '.then is a function')
+    t.end()
+  })
 
+  t.test('- fetchCommentsFragment() rejects promise if required parameters are missing', t => {
+    return fetchCommentsFragment()
+    .then(() => t.fail('promise should not resolve'))
+    .catch((err) => {
+      t.ok(err, 'promise is rejected with an error')
+    })
+    .then(() => fetchCommentFragment('videoId'))
+    .then(() => t.fail('promise should not resolve'))
+    .catch((err) => {
+      t.ok(err, 'promise is rejected with an error')
+    })
+    .then(() => fetchCommentFragment('videoId', {}))
+    .then(() => t.fail('promise should not resolve'))
+    .catch((err) => {
+      t.ok(err, 'promise is rejected with an error')
+    })
+  })
+
+  t.test('- fetchCommentsFragment() rejects promise if session tokens are missing', t => {
+    return fetchCommentsFragment('videoId', {}, () => {})
+    .then(() => t.fail('promise should not resolve'))
+    .catch((err) => {
+      t.ok(err, 'promise is rejected with an error')
+    })
+    .then(() => fetchCommentFragment('videoId', {sessionToken: 'token'}, () => {}))
+    .then(() => t.fail('promise should not resolve'))
+    .catch((err) => {
+      t.ok(err, 'promise is rejected with an error')
+    })
+    .then(() => fetchCommentFragment('videoId', {commentsToken: 'token'}, () => {}))
+    .then(() => t.fail('promise should not resolve'))
+    .catch((err) => {
+      t.ok(err, 'promise is rejected with an error')
+    })
+  })
+
+
+  t.test('- fetchCommentsFragment() fetches a comments fragment', t => {
+    const videoId = 'K3rJ5kK52'
+    const sessionToken = 'the_session_token'
+    const commentsToken = 'the_comments_token'
+    const session = {
+      sessionToken,
+      commentsToken
+    }
+
+    const url = buildWatchFragmentsUrl(videoId, commentsToken)
+    const request = sinon.stub().returns(Promise.resolve())
+
+    return fetchCommentsFragment(videoId, session, request)
+      .then(() => {
+        t.ok(request.calledOnce, 'request called once')
+        t.deepEqual(request.firstCall.args[0], {
+          method: 'POST',
+          json: true,
+          form: {session_token: sessionToken},
+          url
+        }, 'request called with correct parameters')
+      })
+  })
+
+  t.test('- extractPageToken() throws an error on missing parameter', t => {
+    t.throws(() => extractPageToken(), 'throws an error')
+    t.end()
+  })
+
+  t.test('- extractPageToken() throws an error if watch-discussion is missing from response', t => {
+    t.throws(() => extractPageToken({'nothing': 'here'}), 'throws an error')
+    t.end()
+  })
+
+  t.test('- extractPageToken() throws an error if the page token cannot be found', t => {
+    const html = '<div><div class="yadayada" token="blahblah">content</div></div>'
+    t.throws(() => extractPageToken({'watch-discussion': html}))
+    t.end()
+  })
+
+  t.test('- extractPageToken() does not extract the "Load more" page token', t => {
+    const html = [
+      '<div>',
+      '<button class="yt-uix-button yt-uix-button-size-default yt-uix-button-default load-more-button yt-uix-load-more comment-section-renderer-paginator yt-uix-sessionlink selectorgadget_suggested" type="button" onclick=";return false;" aria-label="Show more" data-sessionlink="itct=CAEQuy8iEwjk_-eHu4XPAhXKL04KHeSwCbI" data-uix-load-more-post-body="page_token=EhYSCzJhNFV4ZHk5VFFZwAEAyAEA4AEBGAYywgIKrgJDZzBRbm9ic2g3dUZ6d0lnQUNnQkVzSUJDQUFROEx5djhxcUZ6d0lxdEFIcnBaRGNtNXE2N3p1NTA1bWotTkdvd0R2aWdfX3EyOFgtcERpSTFZdUY3b3Vka1RuRm9NclM3cEtkdkRxT3JxWF9zcWp5eFRuTTZZeTU3SmJMNkRxNnh0cWVsY2I2cXp1VDhaLXc0b3JLOURpUXRMdnEtdHZydlRpUjM3eVJ5THlOdXp2R3Z0UHBnN2plX1R1aXItdUR3OFM1NER2ZjdNRzloT2ZuNlRtXzM2dlBzZkNmZ3pqSF82cTUyZGUyOWpuTDFQNmRudU9tM2pyOXplM0o1WVdDbGppMjVvbWZsY3I1LVRpanVzZkE5N3VwOVRvWUFTQVVLS3o1LThtV2c5S0JMZyIPIgsyYTRVeGR5OVRRWTAA" data-uix-load-more-post="true" data-uix-load-more-href="/comment_service_ajax?action_get_comments=1" data-sessionlink-target="/comment_service_ajax?action_get_comments=1" data-uix-load-more-target-id="comment-section-renderer-items">',
+      'Loading More',
+      '</button>',
+      '<div>'
+    ].join('')
+
+    t.throws(() => extractPageToken({'watch-discussion': html}), 'throws an error')
+    t.end()
+  })
+
+  t.test('- extractPageToken() extracts the correct page token', t => {
+    const pageToken = 'EhYSCzJhNFV4ZHk5VFFZwAEAyAEA4AEBGAYyESIPIgsyYTRVeGR5OVRRWTAB'
+    const html = [
+      '<div>div class="comment-section-sort-menu"><div class="yt-uix-menu-content">',
+      '<ul>',
+      '<li>',
+      '<button data-token="EhYSCzJhNFV4ZHk5VFFZwAEAyAEA4AEBGAYyESIPIgsyYTRVeGR5OVRRWTAA">false</button>',
+      '</li>',
+      '<li>',
+      '<button data-menu_name="newest-first" data-token="' + pageToken + '">real</button>',
+      '</li>',
+      '</ul>',
+      '</div></div></div>'
+    ].join('')
+
+    const result = extractPageToken({'watch-discussion': html})
+    t.equal(result, pageToken, 'extracts page token correctly')
+    t.end()
+  })
+
+  t.test('- module fetches the first page token', t => {
+    const videoId = 'K3rJ5kK52'
+    const sessionToken = 'the_session_token'
+    const commentsToken = 'the_comments_token'
+    const session = {
+      sessionToken,
+      commentsToken
+    }
+    const pageToken = 'EhYSCzJhNFV4ZHk5VFFZwAEAyAEA4AEBGAYyESIPIgsyYTRVeGR5OVRRWTAB'
+    const html = [
+      '<div>div class="comment-section-sort-menu"><div class="yt-uix-menu-content">',
+      '<ul>',
+      '<li>',
+      '<button data-token="EhYSCzJhNFV4ZHk5VFFZwAEAyAEA4AEBGAYyESIPIgsyYTRVeGR5OVRRWTAA">false</button>',
+      '</li>',
+      '<li>',
+      '<button data-menu_name="newest-first" data-token="' + pageToken + '">real</button>',
+      '</li>',
+      '</ul>',
+      '</div></div></div>'
+    ].join('')
+
+    const url = buildWatchFragmentsUrl(videoId, commentsToken)
+    const request = sinon.stub().returns(Promise.resolve({'watch-discussion': html}))
+
+    return fetchFirstPageToken(videoId, session, { request })
+      .then(result => t.equal(result, pageToken, 'fetches correct page token'))
+  })
 })
