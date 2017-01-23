5e025dcd0b0f8724e832fd2885f2f3f5add5a576
Philip Klostermann <philip.klostermann@gmail.com>
2017-01-23 15:57:03 -0500

    fix example

--- example.js
+++ example.js
@@ -6,7 +6,7 @@ const fetchAllComments = (videoId, pageToken, fetched = []) =>
     .chain(({ comments, nextPageToken }) =>
       nextPageToken
         ? fetchAllComments(videoId, nextPageToken, fetched.concat(comments))
-        : Task.of(comments))
+        : Task.of(fetched.concat(comments)))
 
 fetchAllComments('h_tkIpwbsxY')
   .fork(e => console.error('ERROR', e),
