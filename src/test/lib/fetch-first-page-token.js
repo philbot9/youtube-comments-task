4db48301698b77f58cb8ea7508cf002081a07c93
Philip Klostermann <philip.klostermann@gmail.com>
2018-10-25 18:02:40 -0400

    add test for a single comment

--- src/test/lib/fetch-first-page-token.js
+++ src/test/lib/fetch-first-page-token.js
@@ -59,6 +59,53 @@ describe('/lib/fetch-first-page-token.js', () => {
     )
   })
 
+  it('still succeeds if there is only one comment', done => {
+    const videoId = 'videoId'
+    const pageToken =
+      'EhYSC2hfdGtJcHdic3hZwAEAyAEA4AEBGAYyEyIPIgtoX3RrSXB3YnN4WTABMAA%3D'
+    const encodedPageToken = encodeURIComponent(pageToken)
+    const html = `
+      <div class="comment-section-renderer">
+        <h2 class="comment-section-header-renderer" tabindex="0">
+          <b>Comment</b> • 1<span class="alternate-content-link"></span>
+        </h2>
+        <div class="yt-uix-menu comment-section-sort-menu">
+          <button class="yt-uix-button yt-uix-button-size-default" type="button">Button</button>
+          <div class="yt-uix-menu-content yt-ui-menu-content" role="menu"
+            <ul tabindex="0" class="yt-uix-kbd-nav yt-uix-kbd-nav-list">
+              <li>
+                <button type="button" class="yt-ui-menu-item comment-section-sort-menu-item" data-token="WROOOONG" data-menu_name="n/a">
+                  Not it
+                </button>
+              </li>
+              <li>
+                <button type="button" class="yt-ui-menu-item comment-section-sort-menu-item" data-token="${encodedPageToken}" data-menu_name="n/a">
+                  Newest First
+                </button>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>`
+
+    const Youtube = td.replace('../../lib/youtube-api/youtube-api')
+    const fetchFirstPageToken = require('../../lib/fetch-first-page-token')
+
+    td
+      .when(Youtube.commentsWatchFragment(videoId))
+      .thenReturn(Task.of({ body: { 'watch-discussion': html } }))
+
+    fetchFirstPageToken(videoId).fork(
+      e => {
+        done(`ERROR: ${JSON.stringify(e, null, 2)}`)
+      },
+      res => {
+        expect(res).to.equal(pageToken)
+        done()
+      }
+    )
+  })
+
   it('task fails if API request fails', done => {
     const videoId = 'videoId'
     const errMessage = 'API request failed'
