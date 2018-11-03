87613274ee3003ba5d361946ee355892b8b57427
Philip Klostermann <philip.klostermann@gmail.com>
2018-11-03 12:02:36 -0400

    FIX: comment replies on YouTube are now paginated

--- src/lib/fetch-replies.js
+++ src/lib/fetch-replies.js
@@ -1,9 +1,10 @@
 const Task = require('data.task')
 const Either = require('data.either')
 const debug = require('debug')('fetch-replies')
+const { liftM2 } = require('control.monads')
 
 const { commentReplies } = require('./youtube-api/youtube-api')
-const { cheerio } = require('./utils/cheerio-utils')
+const { cheerio, cheerioFindAttr } = require('./utils/cheerio-utils')
 const parseReplies = require('./parse-replies')
 const { scraperError } = require('./error-handler')
 
@@ -27,22 +28,46 @@ const parseCommentReplies = $replies =>
     })
     .fold(Task.rejected, Task.of)
 
-const fetchReplies = (videoId, comment) =>
-  getRepliesToken(comment)
-    .chain(repliesToken => commentReplies(videoId, repliesToken))
+const extractNextRepliesToken = $content =>
+  cheerioFindAttr($content, '.load-more-button', 'data-uix-load-more-post-body')
+    .map(token => token.replace(/^page_token=/i, ''))
+    .map(decodeURIComponent)
+    .orElse(() => Either.of(null))
+    .fold(Task.rejected, Task.of)
+
+const fetchAllReplies = (videoId, repliesToken) =>
+  commentReplies(videoId, repliesToken)
     .chain(getContentHtml)
     .map(html => cheerio(`<div>${html}</div>`))
-    .chain(parseCommentReplies)
+    .chain($replies =>
+      liftM2(
+        (replies, nextRepliesToken) => ({ replies, nextRepliesToken }),
+        parseCommentReplies($replies),
+        extractNextRepliesToken($replies)
+      )
+    )
+    .chain(
+      ({ replies, nextRepliesToken }) =>
+        (nextRepliesToken
+          ? fetchAllReplies(videoId, nextRepliesToken).map(nextReplies =>
+              replies.concat(nextReplies)
+            )
+          : Task.of(replies))
+    )
+
+const fetchReplies = (videoId, comment) =>
+  getRepliesToken(comment)
+    .chain(repliesToken => fetchAllReplies(videoId, repliesToken))
     .rejectedMap(
       e =>
-        e.type
+        (e.type
           ? e
           : scraperError({
-              videoId,
-              message: e,
-              component: 'fetch-replies',
-              operation: 'fetch-replies'
-            })
+            videoId,
+            message: e,
+            component: 'fetch-replies',
+            operation: 'fetch-replies'
+          }))
     )
 
 module.exports = fetchReplies
--- src/test/lib/fetch-replies.js
+++ src/test/lib/fetch-replies.js
@@ -166,4 +166,95 @@ describe('/lib/fetch-first-page-token.js', () => {
       }
     )
   })
+
+  it('Fetches multiple pages of replies', done => {
+    const videoId = 'videoId'
+    const repliesToken = 'repliesToken'
+    const nextRepliesToken = 'nextRepliesToken%3asd'
+
+    const repliesPage1 = [
+      {
+        id: 'commentid.reply1id',
+        author: 'reply1_author',
+        authorLink: 'reply1_author_link',
+        authorThumb: 'reply1_author_thumb',
+        text: 'reply1_text',
+        likes: 10,
+        time: '10 hours ago',
+        timestamp: parseInt(moment().subtract(10, 'hours').format('x'), 10)
+      },
+      {
+        id: 'commentid.reply2id',
+        author: 'reply2_author',
+        authorLink: 'reply2_author_link',
+        authorThumb: 'reply2_author_thumb',
+        text: 'reply2_text',
+        likes: 0,
+        time: '2 minutes ago',
+        timestamp: parseInt(moment().subtract(2, 'minutes').format('x'), 10)
+      }
+    ]
+
+    const repliesPage2 = [
+      {
+        id: 'commentid.reply3id',
+        author: 'reply3_author',
+        authorLink: 'reply3_author_link',
+        authorThumb: 'reply3_author_thumb',
+        text: 'reply3_text',
+        likes: 10,
+        time: '10 hours ago',
+        timestamp: parseInt(moment().subtract(10, 'hours').format('x'), 10)
+      },
+      {
+        id: 'commentid.reply4id',
+        author: 'reply4_author',
+        authorLink: 'reply4_author_link',
+        authorThumb: 'reply4_author_thumb',
+        text: 'reply4_text',
+        likes: 0,
+        time: '2 minutes ago',
+        timestamp: parseInt(moment().subtract(2, 'minutes').format('x'), 10)
+      }
+    ]
+
+    const page1Html =
+      sampleReplies(repliesPage1) +
+      `<button 
+        class="load-more-button" 
+        data-uix-load-more-post-body="page_token=${encodeURIComponent(nextRepliesToken)}">
+        Load more
+      </button>`
+
+    const page2Html = sampleReplies(repliesPage2)
+
+    const Youtube = td.replace('../../lib/youtube-api/youtube-api')
+    const fetchReplies = require('../../lib/fetch-replies')
+
+    td.when(Youtube.commentReplies(videoId, repliesToken)).thenReturn(
+      Task.of({
+        content_html: page1Html
+      })
+    )
+
+    td.when(Youtube.commentReplies(videoId, nextRepliesToken)).thenReturn(
+      Task.of({
+        content_html: page2Html
+      })
+    )
+
+    fetchReplies(videoId, { repliesToken }).fork(
+      e => {
+        expect.fail(e)
+        done(e)
+      },
+      result => {
+        const allReplies = repliesPage1.concat(repliesPage2)
+
+        expect(result).to.be.an('array').of.length(4)
+        result.forEach((r, i) => validateComment(r, allReplies[i]))
+        done()
+      }
+    )
+  })
 })
